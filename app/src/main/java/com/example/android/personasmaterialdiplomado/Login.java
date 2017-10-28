6298747f85a589e02db5048ce52f048c6086216a
“Gilbertog12” <gibertogalindo1@gmail.com>
2017-10-28 14:53:24 -0500

    Autenticacion Terminada

--- app/src/main/java/com/example/android/personasmaterialdiplomado/Login.java
+++ app/src/main/java/com/example/android/personasmaterialdiplomado/Login.java
@@ -1,10 +1,18 @@
 package com.example.android.personasmaterialdiplomado;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.storage.FirebaseStorage;
 
 public class Login extends AppCompatActivity {
@@ -17,6 +25,68 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        email = (EditText)findViewById(R.id.txtemail);
+        password=(EditText)findViewById(R.id.txtcontrasena);
+        firebaseAuth = firebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener(){
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+            }
+        };
+
+
+
     }
-    
+
+    public  void singin(String email, String password){
+        firebaseAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Intent i = new Intent(Login.this,Principal.class);
+                    startActivity(i);
+                    finish();
+                }else {
+                    Toast.makeText(Login.this,task.getException().getMessage(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+
+    public void crearUsuario(String email, String password){
+        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(Login.this,R.string.mensaje_cuenta_creada_exitosamente,Toast.LENGTH_SHORT);
+                }else{
+                    Toast.makeText(Login.this,task.getException().getMessage(),Toast.LENGTH_LONG);
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseAuth.removeAuthStateListener(authStateListener);
+    }
+
+    public void ingresar(View V){
+        singin(email.getText().toString(),password.getText().toString());
+    }
+
+    public  void crear(View V){
+        crearUsuario(email.getText().toString(),password.getText().toString());
+    }
 }
